451bace33b160771656beae338ae4ea5e8c31004
Takanori Hirohashi <chashi.design@gmail.com>
2019-02-01 22:50:45 +0900

    test

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>chashihahaha</Text>
+        <Text style={styles.welcome}>chashihahahaha</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
